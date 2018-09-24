3f2567ff1ff2c9b525381029ea246e066968631c
vaibhav <vbvrocks84@gmail.com>
2018-09-23 21:31:14 -0500

    added cors support

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from chatterbot import ChatBot
+from flask_cors import CORS, cross_origin
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-english_bot = ChatBot("English Bot", 
+english_bot = ChatBot("English Bot",
                      storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
                      database = "chatterbot",
                      database_uri = "mongodb://vmal:admin123@ds127490.mlab.com:27490/chatterbot")
@@ -13,10 +16,12 @@ english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
 
 @app.route("/")
+@cross_origin()
 def home():
     return "Hello!!!!"
 
 @app.route("/getReply", methods = ['POST'])
+@cross_origin()
 def get_response():
     req_data = request.get_json()
     print(req_data)
